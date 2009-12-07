6383c13b9b134d07a6cbd8e185651309584310e7
setiapramana <setiapramana@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-12-07 10:16:55 +0000

    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/isogenegui@12 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/onHelpIsoGeneGUI.R
+++ pkg/R/onHelpIsoGeneGUI.R
@@ -1,9 +1,7 @@
 `onHelpIsoGeneGUI` <-
 function() {
-                tkgrab.release(window)
-    helpIndex <- file.path(system.file("doc",package="IsoGeneGUI2"),"index.html")
-   #helpIndex <- ("C:\\Documents and Settings\\lucp1898\\My Documents\\helpGUIhtml\\help\\index.html")
-
-                 browseURL(helpIndex)
-                }
+          tkgrab.release(window)
+    	    helpIndex <- file.path(system.file("doc",package="IsoGeneGUI"),"index.html")
+          browseURL(helpIndex)
+            }
 
